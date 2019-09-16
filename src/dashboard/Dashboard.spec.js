5057b9cfe1cfac65b3be742ae1f706d95670239f
Kelly Moreira <moreira.kd@outlook.com>
2019-09-16 17:09:35 -0400

    failures

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,28 @@
 // Test away
+
+import React from 'react';
+import Dashboard from './Dashboard.js';
+
+import '@testing-library/react';
+import { render, fireEvent } from 'react-testing-library';
+import renderer from 'react-test-renderer'
+
+
+describe('<Dashboard />', () => {
+
+    it('should match snapshot', () => {
+        const tree = renderer.create(<Dashboard />).toJSON;
+        expect(tree).toMatchSnapshot();
+    });
+
+    it('should first diplay open and unlocked', () =>{
+        const {getByText} = render(<Dashboard />)
+        getByText(/open/i)
+        getByText(/unlocked/i) 
+    })
+ 
+   
+})
+
+
+   
